919ea6e8aab8d4c788853c5760c0487fcfc62d2d
Bob100ilovski <47972510+Bob100ilovski@users.noreply.github.com>
2019-03-20 17:24:24 +0100

    Add files via upload

---
+++ hw 05/hw 05/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // array
+            Student[] student = new Student[5];
+
+            // for for create students
+            for (int i = 0; i < student.Length; i++)
+            {
+                student[i] = new Student();
+                Console.Write("Enter name: ");
+                student[i].FirstName = Console.ReadLine();
+                Console.Write("Enter group: ");
+                student[i].Group = Console.ReadLine();
+                Console.Write("Enter academy: ");
+                student[i].Academy = Console.ReadLine();
+            }
+
+            // for for-Intro()
+            for (int j = 0; j < student.Length; j++)
+            {
+                Console.WriteLine($"Student {j+1}: {student[j].Intro().ToUpper()}");
+            }
+
+            
+
+            Console.Read();
+        }
+    }
+}
---
+++ hw 05/hw 05/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_05
+{
+    class Student
+    {
+
+        public string FirstName { get; set; }
+        public string Academy { get; set; }
+        public string Group { get; set; }
+
+
+        public string Intro()
+        {
+            return $"{FirstName} from group {Group} academy {Academy}";
+        }
+
+    }
+}
