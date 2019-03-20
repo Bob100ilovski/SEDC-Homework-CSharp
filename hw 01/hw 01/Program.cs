4607bfd6fefcd5fca3f3ad34bf2e8e6a59fa4e64
Bob100ilovski <47972510+Bob100ilovski@users.noreply.github.com>
2019-03-20 17:19:11 +0100

    Add files via upload

---
+++ hw 01/hw 01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //------- Borce Stojilovski - Home work 01------
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("     *");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("    *");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("**");
+            Console.WriteLine("   *****");
+            Console.WriteLine("  *******");
+            Console.WriteLine(" *********");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("     *");
+            Console.WriteLine("     *");
+            Console.WriteLine("     *");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("            ");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Borce Stojilovski G4 - home work 01");
+            
+            // Postaviv i nekoj lampion.
+
+            Console.Read();
+        }
+    }
+}
